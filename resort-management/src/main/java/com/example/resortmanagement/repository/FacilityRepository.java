2ea93cad156355dd8382c58fcb1daec7e932f34e
NIHARIKA-NK <123749184+NIHARIKA-NK@users.noreply.github.com>
2025-04-22 19:03:50 +0530

    Delete resort-management directory

--- resort-management/src/main/java/com/example/resortmanagement/repository/FacilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.resortmanagement.repository;
-
-import com.example.resortmanagement.model.Facility;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacilityRepository extends JpaRepository<Facility, Integer> {
-}
